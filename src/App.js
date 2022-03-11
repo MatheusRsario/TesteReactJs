bd7da024da65fb3cb9920d0ca49586851ba2339d
matheus <matheusfcsrosario@gmail.com>
2022-03-10 22:10:43 -0300

    aplicacao corrigida

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import './reset.css'
 import imgLogo from './img/logo.png'
 import imgLupa from './img/lupa.png'
 import Card from './compoments/Card'
